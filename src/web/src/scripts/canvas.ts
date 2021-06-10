fc0e2078dba7dcf2c5900dc632de03c6a98ba7e6
Jannis Becker <jbecker@jannisbecker.me>
2021-06-10 19:35:24 +0200

    add canvas panning and zoom to cursor

--- src/web/src/scripts/canvas.ts
+++ src/web/src/scripts/canvas.ts
@@ -1,5 +1,6 @@
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
+ctx.imageSmoothingEnabled = false;
 
 let image: HTMLImageElement;
 
@@ -13,8 +14,10 @@ let currentScale: number,
 let panX: number = 0,
     panY: number = 0;
 
+let isPanning = false;
 
-function resizeCanvas() {
+
+function recalculateCanvasSize() {
     canvas.width = canvas.clientWidth;
     canvas.height = canvas.clientHeight;
 }
@@ -22,58 +25,79 @@ function resizeCanvas() {
 function loadImage(newImage: HTMLImageElement) {
     image = newImage;
 
-    minScale =  canvas.height / image.naturalHeight;
+    minScale = canvas.height / image.naturalHeight;
     currentScale = minScale;
 
     maxScale = 1;
+
+    panX = 0;
+    panY = 0;
 
 
     redraw();
 }
 
+function recalculateFollowMousePanDistance(newScale) {
+    const canvasBounds = canvas.getBoundingClientRect();
+
+    // get mouse position relative to canvas element
+    const mouseXCanvas = mouseX - canvasBounds.left;
+    const mouseYCanvas = mouseY - canvasBounds.top;
+
+    // get mouse position relative to the center of the image,
+    // which is centered on the canvas minus the current panning
+    const mouseXImage = mouseXCanvas - (canvas.width / 2) - panX;
+    const mouseYImage = mouseYCanvas - (canvas.height / 2) - panY;
+
+    // pan is (old rel. dist from mouse to center of image / old scaling factor) = (new rel. dist. from mouse to center of image / new scaling factor)
+    // solved for the new rel. distance.
+    // since we need the difference between old and new relative distance (which inversed is the distance we need to adjust the image to),
+    // we simply subtract the old relative distance
+    panX -= ((mouseXImage/currentScale) * newScale) - mouseXImage
+    panY -= ((mouseYImage/currentScale) * newScale) - mouseYImage
+
+
+}
+
 function zoomIn() {
-    if(currentScale + scaleStep <= maxScale) {
-        currentScale += scaleStep;
+    const newScale = currentScale + scaleStep
 
-        const mXCenter = mouseX - (canvas.width / 2);
-        const mYCenter = mouseY - (canvas.height / 2);
-        // panX = mXCenter * currentScale;
-        // panY = mYCenter * currentScale;
-
+    if (newScale <= maxScale) {
+        recalculateFollowMousePanDistance(newScale);
+        currentScale = newScale;
 
         redraw();
     }
 }
 
 function zoomOut() {
-    if(currentScale - scaleStep >= minScale) {
-        currentScale -= scaleStep;
+    const newScale = currentScale - scaleStep
 
-        const mXCenter = mouseX - (canvas.width / 2);
-        const mYCenter = mouseY - (canvas.height / 2);
-        // panX = mXCenter * currentScale;
-        // panY = mYCenter * currentScale;
+    if (newScale >= minScale) {
+        recalculateFollowMousePanDistance(newScale);
+        currentScale = newScale;
 
         redraw();
     }
 }
 
 function redraw() {
-    ctx.setTransform(1, 0, 0, 1, 0, 0);
-    ctx.clearRect(0,0, canvas.width, canvas.height);
+    window.requestAnimationFrame(() => {
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
 
-    const tX = (canvas.width / 2) - ((currentScale * image.naturalWidth) / 2) + panX;
-    const tY = (canvas.height / 2) - ((currentScale * image.naturalHeight) / 2) + panY;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    console.log({tX,tY})
+        const tX = (canvas.width / 2) - ((currentScale * image.naturalWidth) / 2) + panX;
+        const tY = (canvas.height / 2) - ((currentScale * image.naturalHeight) / 2) + panY;
 
-    ctx.transform(currentScale,0,0,currentScale, tX, tY)
+        ctx.transform(currentScale, 0, 0, currentScale, tX, tY)
 
-    ctx.drawImage(image, 0, 0);
+        ctx.drawImage(image, 0, 0);
+    })
 }
 
 
-resizeCanvas();
+recalculateCanvasSize();
 const fileInput = document.getElementById('file-input') as HTMLInputElement;
 
 fileInput.onchange = (e) => {
@@ -90,16 +114,41 @@ fileInput.onchange = (e) => {
 canvas.addEventListener('wheel', (e: WheelEvent & { wheelDelta: number }) => {
     const delta = e.wheelDelta ?? e.deltaY
 
-    if(delta > 0) {
+    if (delta > 0) {
         zoomIn();
     } else {
         zoomOut();
     }
 })
 
+canvas.addEventListener('mousedown', e => {
+  if(e.button === 1) {
+      isPanning = true;
+  }
+})
+
+canvas.addEventListener('mouseup', e => {
+    console.log(e.button);
+
+  if(e.button === 1) {
+      isPanning = false;
+  }
+})
+
 canvas.addEventListener('mousemove', e => {
-    mouseX = e.pageX;
-    mouseY = e.pageY;
+    mouseX = e.pageX
+    mouseY = e.pageY
+
+    if(isPanning) {
+        panX += e.movementX;
+        panY += e.movementY;
+
+        redraw();
+    }
+})
+
+canvas.addEventListener('drag', e => {
+
 })
 
 
